f4b73ac4caa454535b3aae406f3b0b167d65c05f
Hafizh Ibnu Syam <aqrashu.asus@gmail.com>
2024-03-19 11:45:27 +0700

    Add vehicle approver fields to RepairOrder model
    This commit adds two fields `approver_1_id` and `approver_2_id` to the `RepairOrder` model in order to store the information of the approvers for repair orders. These fields are used to track the approvers in the repair report and maintenance report templates. The default values for `approver_1_label` and `approver_2_label` are set as "Kepala Mekanik" (Head Mechanic) and "Operations SPV" (Operations Supervisor), respectively. This change improves the tracking and management of repair orders by including the names of the approvers in the reports.

--- mtr_custom/models/repair.py
+++ mtr_custom/models/repair.py
@@ -8,6 +8,10 @@ class RepairOrder(models.Model):
     vehicle_id = fields.Many2one(comodel_name='fleet.vehicle', string='Jenis Alat / Kendaraan')
     vehicle_type_id = fields.Many2one(comodel_name='fleet.vehicle.model', string='Type / Merk', related='vehicle_id.model_id')
     no_pol = fields.Char(string='No. Pol / Unit', related='vehicle_id.license_plate')
+    approver_1_id = fields.Many2one(comodel_name='res.users', string='Approver 1')
+    approver_1_label = fields.Char(string='Approver 1 Label')
+    approver_2_id = fields.Many2one(comodel_name='res.users', string='Approver 2')
+    approver_2_label = fields.Char(string='Approver 2 Label')
 
 
 class RepairLine(models.Model):
